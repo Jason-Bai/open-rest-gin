483a3408aa004a6b623e92ffa66e9054b56c7bf2
baiyu <baiyu@admaster.com.cn>
2018-07-06 14:55:22 +0800

    add utils/config.go

---
+++ utils/config.go
@@ -0,0 +1,67 @@
+package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Config struct {
+	App struct {
+		Name string `json:"name"`
+		Desc string `json:"desc"`
+		Host string `json:"host"`
+		Port string `json:"port"`
+	} `json:"app"`
+
+	Db struct {
+		Host string `json:"host"`
+		Port string `json:"port"`
+		Name string `json:"name"`
+		User string `json:"user"`
+		Pass string `json:"pass"`
+	} `json:"db"`
+
+	Redis struct {
+		Host string `json:"host"`
+		Port string `json:"port"`
+	} `json:"redis"`
+
+	OAuth struct {
+		URL string `json:"url"`
+	}
+}
+
+func GetConfig() Config {
+	cfg := Config{}
+
+	cfgDir := os.Getenv("CONFIGDIR")
+
+	if cfgDir == "" {
+		cfgDir = "./configs"
+	}
+
+	env := os.Getenv("ENV")
+
+	if env == "" {
+		env = gin.DebugMode
+	}
+
+	cfgFile := path.Join(cfgDir, "config."+env+".json")
+
+	if _, err := os.Stat(cfgFile); os.IsNotExist(err) {
+		log.Fatal("no config file", err)
+	}
+
+	if content, err := ioutil.ReadFile(cfgFile); err == nil {
+		if err := json.Unmarshal(content, &cfg); err != nil {
+			log.Fatal("no config file", err)
+		}
+	}
+
+	return cfg
+}
